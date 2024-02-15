8c1653849a7d24c4f8810af75dff6dbbee14022e
Dansarasix-DML <danisarasix454@gmail.com>
2024-02-15 07:00:51 +0100

    1.00c: Comentarios añadidos

--- js/main.js
+++ js/main.js
@@ -1,19 +1,30 @@
 /**
  * @author Daniel Marín López
- * @version 0.01a
+ * @version 1.00c
  */
 
-
+/**
+ * Objeto donde está la lógica entera
+ * del mastermind.
+ */
 const mastermind = (function () {
 
+    //Array donde se almacena el objetivo
     let objetivo = [];
 
+    //Array de colores
     const COLORES = [
         "orange", "limegreen", "deepskyblue",
         "crimson", "yellow", "indigo",
         "aqua", "hotpink"
     ];
 
+    /**
+     * Función que comprueba la combinación en el array
+     * metido por el usuario y devuelve la comprobación.
+     * Si todas las bolas son negras saldrá el mensaje
+     * de victoria en la consola.
+     */
     comprobar = function(intento){
 
         if (intento.length !== 4) {
@@ -42,6 +53,10 @@ const mastermind = (function () {
         return sol.sort();
     }
 
+    /**
+     * Función que inicializa el array objetivo con colores
+     * aleatorios que se pueden repetir
+     */
     init = function(){
         console.log(`
             #################################################
@@ -56,12 +71,19 @@ const mastermind = (function () {
         objetivo = aColores(objetivo);
     }
 
+    /**
+     * Función que muestra el objetivo.
+     * ADVERTENCIA: ESTA FUNCIÓN NO SE DEBERÍA
+     * LLAMAR EN LA VERSIÓN FINAL.
+     */
     mostrar = function () {
         console.log(`Tu objetivo es: ${objetivo}`);
     }
 
+    //Función que mapea el array de números por colores
     const aColores = array => array.map(element => COLORES[element]);
 
+    //Parte pública del mastermind
     return {
         init: init,
         comprobar: comprobar,
@@ -72,6 +94,10 @@ const mastermind = (function () {
 })()
 
 {
+    /**
+     * Función que dehabilita los botones de colores
+     * y hace visible el mensaje de victoria.
+     */
     const victoria = () => {
         let set1 = document.querySelector(".set1").getElementsByTagName("div");
         let set2 = document.querySelector(".set2").getElementsByTagName("div");
@@ -85,6 +111,13 @@ const mastermind = (function () {
         initSound2();
     }
     
+    /**
+     * Función de los botones de colores para asignar su color
+     * a una casilla. Para ello se recogen los intentos y se busca
+     * aquel con el data-flag === 0. Una vez encontrado hacemos el
+     * mismo procedimiento pero con las casillas y buscamos aquella
+     * cuyo data-color esté vacío para asignarle el color.
+     */
     const giveColor = function () {
         let intentos = [...document.querySelectorAll(".intento")];
 
@@ -101,6 +134,10 @@ const mastermind = (function () {
         }
     }
 
+    /**
+     * Función que utilizan las casillas para eliminar
+     * el color asignado por el jugador anteriormente.
+     */
     const deleteColor = function() {
         if (!!this.getAttribute("data-color")) {
             this.classList.remove(this.getAttribute("data-color"));
@@ -108,12 +145,25 @@ const mastermind = (function () {
         }
     }
 
+    /**
+     * Función para dar comportamiento a las casillas iniciales.
+     */
     const behaviourDivs = () => {
         let cells = [...document.querySelector(".part1").querySelectorAll("div")];
 
         cells.forEach(div => div.addEventListener("click", deleteColor));
     }
 
+    /**
+     * Función que comprueba la combinación del jugador
+     * usando el comprobar(colors) del objeto mastermind.
+     * El flag que devuelve se usará para indicar cuando
+     * dar el mensaje de victoria y dehabilitarlo todo.
+     * 
+     * @param {*} intento HTMLElement
+     * @param {*} colors Array
+     * @returns boolean
+     */
     const comprobación = (intento, colors) => {
         let cells = [...intento.querySelector(".part2").querySelectorAll("div")];
         const resultado = comprobar(colors);
@@ -128,8 +178,23 @@ const mastermind = (function () {
 
     }
 
+    /**
+     * Función para asignarle comportamiento al
+     * botón de confirmación (✔️)
+     */
     const behaviourConf = (clone) => {
 
+        /**
+         * Función que recoge los intentos y mediante el data-flag
+         * busca el que no haya cambiado (mucho más cómodo que recoger
+         * todo de golpe). Una vez encontrado, deshabilita las casillas
+         * y comprueba la combinación.
+         * 
+         * Si es la combinación ganadora, el botón de deshabilita a si
+         * mismo y llama a victoria(), si no añade un clon del 
+         * div original (nuevo intento). En ambos casos se pone el
+         * data-flag = 1
+         */
         const handleClick = () => {
             let intentos = [...document.querySelectorAll(".intento")];
             
@@ -163,6 +228,10 @@ const mastermind = (function () {
 
     }
 
+    /**
+     * Función para asignarle comportamiento a los
+     * botones de colores
+     */
     const behaviourButtons = () => {
         let set1 = document.querySelector(".set1").getElementsByTagName("div");
         let set2 = document.querySelector(".set2").getElementsByTagName("div");
@@ -172,17 +241,30 @@ const mastermind = (function () {
         buttons.forEach(button => button.addEventListener("click", giveColor));
     }
 
+    /**
+     * Funciones para asignarle comportamiento a 
+     * los botones de victoria, no es recomendable 
+     * usar un callback perode bido a que los 
+     * botones no se pueden clicar no pasa nada
+     */
     const behaviouVictory = () => {
         document.querySelector(".iniciar").addEventListener("click", () => location.reload());
         document.querySelector(".cerrar").addEventListener("click", () => document.querySelector(".victoria").style.display = "none");
     }
 
+
+    /**
+     * Funciones de Ester Eggs
+     */
     const initSound = () => {
         document.querySelector(".head1").addEventListener("click", () => document.querySelector(".audio1").play());
     }
 
     const initSound2 = () => document.querySelector(".audio2").play()
 
+    /**
+     * Función para iniciar el comportamiento
+     */
     const iniciar = function() {
         const cloneIntento = document.querySelector(".intento").cloneNode(true);
         initSound();
